08ec214d92e992453615bdbc00049e86038a444e
Rishi-Kant <kant3397@gmail.com>
2023-04-29 00:14:59 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Twitter clone</h1>
+    </>
+  )
+}
+
+export default App
